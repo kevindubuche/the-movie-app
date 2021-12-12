85df7a0d89f1218bdc3de8a5512131bd4bb38931
Kevin Dubuche <kevindubuche@gmail.com>
2021-12-12 16:04:29 +0100

    #1 Done

--- app/src/main/java/com/gmail/eamosse/imdb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/home/HomeFragment.kt
@@ -9,37 +9,39 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
 import com.gmail.eamosse.imdb.R
+import com.gmail.eamosse.imdb.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by viewModel()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.token.observe(viewLifecycleOwner, Observer {
-            textView.text = "${it.requestToken} - ${it.expiresAt}"
-        })
-
-        homeViewModel.error.observe(viewLifecycleOwner, Observer {
-            textView.text = "Erreur $it"
-        })
-        return root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<View>(R.id.button_home).setOnClickListener {
+        homeViewModel.token.observe(viewLifecycleOwner, Observer {
+            binding.textHome.text = "${it.requestToken} - ${it.expiresAt}"
+        })
+
+        homeViewModel.error.observe(viewLifecycleOwner, Observer {
+            binding.textHome.text = "Erreur $it"
+        })
+
+        binding.buttonHome.setOnClickListener {
             val action = HomeFragmentDirections
-                    .actionHomeFragmentToHomeSecondFragment("From HomeFragment")
+                .actionHomeFragmentToHomeSecondFragment("From HomeFragment")
             NavHostFragment.findNavController(this@HomeFragment)
-                    .navigate(action)
+                .navigate(action)
         }
     }
 }
